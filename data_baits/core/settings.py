78681519abc632f303ee7ee4efe0e04e07a2b0ea
Michał Mazurek <mmazurekgda@gmail.com>
2024-03-24 21:43:02 +0100

    Restructure and extract core settings

--- data_baits/core/settings.py
+++ data_baits/core/settings.py
@@ -1,30 +1,4 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from enum import Enum
-import os
-from data_baits import __version__
-
-
-class Environments(str, Enum):
-    dev = "development"
-    prod = "production"
-
-
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_prefix="DATA_BAITS_",
-        env_file=os.getenv("DATA_BAITS_ENV_FILE", ".env"),
-        extra="allow",
-        case_sensitive=True,
-    )
-
-    PROJECT_NAME: str = "data-baits"
-    PROJECT_VERSION: str = __version__
-    DASH_SECRET_KEY: str = ""
-    ENVIRONMENT: Environments = Environments.dev.value
-    K8_NAMESPACE: str = "github-cd"
-    LOGGER_NAME: str = "DB"
-    DEFAULT_STORAGE_CLASS: str = "microk8s-hostpath"
-    LIST_PIPELINES_LIMIT: int = 1000
-
+from data_baits.core.core_settings import Settings, Environments
 
 settings = Settings()
+Environments = Environments  # for backwards compatibility
